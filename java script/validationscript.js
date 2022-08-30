7a67ef3878f8dbfe1dab810313a00770df3f8f4c
snehalprajapati29 <snehalprajapati.1991@gmail.com>
2022-08-29 17:02:13 -0700

    validation form update

--- java script/validationscript.js
+++ java script/validationscript.js
@@ -1,44 +1,46 @@
 
       function check(){
-      let fname=document.form1.fname.value
+        //first name validation
+    let fname=document.form1.fname.value
         if(fname=="" || fname==null){
            document.getElementById("p1").innerHTML="Please Enter your first name"
             document.form1.fname.focus()
             return false
         }
-    
+     //second name validation
         let sname=document.form1.sname.value
         if(sname=="" || sname==null){
             document.getElementById("p2").innerHTML="Please Enter your Second name"
             document.form1.fname.focus()
             return false
         }
-    
+     //last name validation
         let lname=document.form1.lname.value
         if(lname=="" || lname==null){
             document.getElementById("p3").innerHTML="Please Enter your Last name"
             document.form1.fname.focus()
             return false
         }
-      
+       //gender validation
             let male=document.form1.gen[0].checked;
             let female=document.form1.gen[1].checked;
             if(male==false && female==false){
                 document.getElementById("p5").innerHTML=("Please select Gender");
                 return false
             }
-
-            let c1=document.form1.c1.value
-            let c2=document.form1.c2.value
-            let c3=document.form1.c3.value
-            let c4=document.form1.c4.value
-            if(c1==false && c2==false && c3==false && c4==false ) 
+ //subject validation
+            let c1=document.form1.c1.checked
+            let c2=document.form1.c2.checked
+            let c3=document.form1.c3.checked
+            let c4=document.form1.c4.checked
+          
+            if(c1 == false && c2 == false && c3 == false && c4 == false ) 
             {
                 document.getElementById("p6").innerHTML=("Please select Subject");
                 return false 
             }
-            
-        var num1=document.form1.password1.value;
+           //password validation  
+      var num1=document.form1.password1.value;
         var num2=document.form1.password2.value;
             if(num1==""){
                 document.getElementById("p4").innerHTML=("Please enter password");
@@ -50,12 +52,14 @@
                 document.form1.password2.focus()
                 return false
             }
+             //city validation
             let city=document.form1.city.value
             if(city== -1){
                 document.getElementById("p7").innerHTML=("select city");
                 document.form1.city.focus()
                 return false
             }
+             //Mobile number validation
             let mno=document.form1.mno.value
             if(mno==""){
                 document.getElementById("p8").innerHTML=("Enter mobile number");
@@ -68,6 +72,7 @@
                 document.form1.mno.focus()
                 return false
             }
+             //Address name validation
             let area=document.form1.add.value
             if(area.length <=20)
             {
@@ -75,6 +80,7 @@
                 document.form1.add.focus()
                 return false
             }
+             // Email validation
             let mail=document.form1.email.value
             if(mail==""){
                 document.getElementById("p9").innerHTML=("Enter Mail Address");
